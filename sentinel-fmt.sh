645ef2967d67ecd7925674ecd9e7849f10c7c7c7
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>
2020-10-30 19:48:09 +0100

    fix: variable appears unused
    Fix SC2034: SCRIPT_DIR appears unused. Verify use (or export if used externally).

--- sentinel-fmt.sh
+++ sentinel-fmt.sh
@@ -3,6 +3,6 @@
 set -o errexit
 # set -o xtrace
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+# SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 sentinel fmt -check=true "$(find . -name "*.sentinel" -type f)"
